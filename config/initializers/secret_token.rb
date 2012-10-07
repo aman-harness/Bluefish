d3cfe1084bb1c6b66c16633f014dbb805a1538ae
vnaybrdwj <vnaybrdwj@gmail.com>
2012-10-07 03:25:44 -0700

    onlogin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlogin::Application.config.secret_token = '2d9db008adaa6d6b2ca7a6470c240fcbb524070dace802af70edb6a7bc438b86a0283c8ba14ceed515feaa6b1f6cf0426c593abe5927a9860282d42072661ae9'
